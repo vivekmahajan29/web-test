871d25e4d97a170b7d79aaec21961b52efea282b
Vivek Mahajan <Vivek_Mahajan@external.mckinsey.com>
2018-11-26 20:28:37 +0530

    updating circle ci

--- src/test/java/web/test/ApplicationTestRunner.java
+++ src/test/java/web/test/ApplicationTestRunner.java
@@ -5,6 +5,7 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(tags= {"@create,@update"},features = "src/test/resources/features")
+// tags= {"@create,@update"},
+@CucumberOptions(features = "src/test/resources/features")
 public class ApplicationTestRunner {
 }
